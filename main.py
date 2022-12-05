9eb93b3983b43d7fddc756be8b997ac34ec00ad0
mkaihan <80855938+mkaihan@users.noreply.github.com>
2022-12-05 12:38:11 -0800

    commit
    commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='School',
+                                         user='root',
+                                         password='')
+    mySql_Create_Table_Query = """CREATE TABLE Laptop ( 
+                             Id int(11) NOT NULL,
+                             Name varchar(250) NOT NULL,
+                             Price float NOT NULL,
+                             Purchase_date Date NOT NULL,
+                             PRIMARY KEY (Id))
+                              CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );"""
+
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Create_Table_Query)
+    print("Laptop Table created successfully ")
+
+except mysql.connector.Error as error:
+    print("Failed to create table in MySQL: {}".format(error))
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
